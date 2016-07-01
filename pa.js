e9a7569c37d9eefab10f998c630d221c31fec2b2
Willie Lawrence <cptx032@gmail.com>
2016-07-01 17:21:01 -0300

    add toast

--- pa.js
+++ pa.js
@@ -7,89 +7,107 @@ Email: cptx032 arroba gmail dot com
 /*
 get by id
 */
-function _(id)
-{
+function _(id) {
 	return document.getElementById(id);
 }
 
-function absolutize()
-{
+/*
+updates just a single item
+*/
+function pa_update_elem( elem ) {
 	var W_HEIGHT = window.innerHeight;
 	var W_WIDTH = window.innerWidth;
+	var dom_elem = _( elem.id );
+	var s = dom_elem.style;
+	s.position = 'absolute';
+	if ( elem.width != undefined ) {
+		s.width = (W_WIDTH * elem.width) + 'px';
+	}
+	else {
+		s.width = null;
+	}
 	
+	if ( elem.height != undefined ) {
+		s.height = (W_HEIGHT * elem.height) + 'px';
+	}
+	else {
+		s.height = null;
+	}
+	
+	if ( elem.left != undefined ) {
+		s.left = (W_WIDTH * elem.left) + 'px';
+	}
+	else {
+		s.left = null;
+	}
+	
+	if ( elem.right != undefined ) {
+		s.right = (W_WIDTH * elem.right) + 'px';
+	}
+	else {
+		s.right = null;
+	}
+	
+	if ( elem.bottom != undefined ) {
+		s.bottom = (W_HEIGHT * elem.bottom) + 'px';
+	}
+	else {
+		s.bottom = null;
+	}
+	
+	if ( elem.top != undefined ) {
+		s.top = (W_HEIGHT * elem.top) + 'px';
+	}
+	else {
+		s.top = null;
+	}
+	// font size é baseado no height
+	if ( elem.fontSize != undefined ) {
+		s.fontSize = (W_HEIGHT * elem.fontSize) + 'px';
+	}
+	else {
+		s.fontSize = null;
+	}
+	
+	if ( elem.center_horizontal ) {
+		s.left = (W_WIDTH/2.0) - ((dom_elem.width||dom_elem.clientWidth)/2.0) + 'px';
+	}
+	if ( elem.center_vertical ) {
+		s.top = (W_HEIGHT/2.0) - ((dom_elem.height||dom_elem.clientHeight)/2.0) + 'px';
+	}
+}
+
+/*
+function runned when the page is resized
+*/
+function pa_update_all() {
 	var pa_elems = window.pa_elems;
 	var length = pa_elems.length;
 	var elem;
-	var s; // style
 	while ( length-- )
 	{
-		elem = _( pa_elems[length].id );
-		s = elem.style;
-		s.position = 'absolute';
-		if ( pa_elems[length].width != undefined ) {
-			s.width = (W_WIDTH * pa_elems[length].width) + 'px';
-		}
-		else {
-			s.width = null;
-		}
-		
-		if ( pa_elems[length].height != undefined ) {
-			s.height = (W_HEIGHT * pa_elems[length].height) + 'px';
-		}
-		else {
-			s.height = null;
-		}
-		
-		if ( pa_elems[length].left != undefined ) {
-			s.left = (W_WIDTH * pa_elems[length].left) + 'px';
-		}
-		else {
-			s.left = null;
-		}
-		
-		if ( pa_elems[length].right != undefined ) {
-			s.right = (W_WIDTH * pa_elems[length].right) + 'px';
-		}
-		else {
-			s.right = null;
-		}
-		
-		if ( pa_elems[length].bottom != undefined ) {
-			s.bottom = (W_HEIGHT * pa_elems[length].bottom) + 'px';
-		}
-		else {
-			s.bottom = null;
-		}
-		
-		if ( pa_elems[length].top != undefined ) {
-			s.top = (W_HEIGHT * pa_elems[length].top) + 'px';
-		}
-		else {
-			s.top = null;
-		}
-		// font size é baseado no height
-		if ( pa_elems[length].fontSize != undefined ) {
-			s.fontSize = (W_HEIGHT * pa_elems[length].fontSize) + 'px';
-		}
-		else {
-			s.fontSize = null;
-		}
-		
-		if ( pa_elems[length].center_horizontal ) {
-			s.left = (W_WIDTH/2.0) - ((elem.width||elem.clientWidth)/2.0) + 'px';
-		}
-		if ( pa_elems[length].center_vertical ) {
-			s.top = (W_HEIGHT/2.0) - ((elem.height||elem.clientHeight)/2.0) + 'px';
-		}
+		elem = pa_elems[length];
+		pa_update_elem( elem );
 	}
 }
-function pa_start( elems )
-{
+
+/*
+starts to verify resize event
+*/
+function pa_start( elems ) {
 	window.pa_elems = elems;
-	absolutize();
+	pa_update_all();
 
 	window.addEventListener('resize', function(){
 		// timeout to run in thread
-		setTimeout(absolutize, 1);
+		setTimeout(pa_update_all, 1);
 	});
+}
+
+/*
+add a new item to be managed by Pa
+*/
+function pa_add( elem ) {
+	window.pa_elems.push( elem );
+	pa_update_elem( elem );
 }
---
+++ pa_toast.js
@@ -0,0 +1,84 @@
+/*
+pa.js
+Author: Willie Lawrence
+Email: cptx032 arroba gmail dot com
+*/
+var PA_TOAST_BG = null;
+var PA_TOAST_TEXT = null;
+/*
+returns a div with the toast
+*/
+function pa_get_toast_element(message) {
+	var fill = document.createElement('div');
+	fill.id = 'pa_toast_bg';
+	var s_b = fill.style;
+	s_b.backgroundColor = '#ffffff';
+	var bg_pa = {
+		id: 'pa_toast_bg',
+		width: 1.0, height: 1.0, left: 0.0, right: 0.0
+	};
+	s_b.transition = 'opacity 1s';
+	document.body.appendChild( fill );
+	pa_add( bg_pa );
+
+	var bg = document.createElement('div');
+	bg.id = 'pa_toast';
+	var s = bg.style;
+	s.backgroundColor = '#ffffff';
+	s.opacity = 0;
+	s.textAlign = 'center';
+	s.transition = 'opacity 1s'
+	bg.innerHTML = message;
+	document.body.appendChild( bg );
+	var pa_dict = {
+		id: 'pa_toast',
+		width: 1.0,
+		center_vertical: true,
+		fontSize: 0.05
+	};
+	pa_add( pa_dict );
+}
+
+function pa_show_toast() {
+	PA_TOAST_BG.style.zIndex = 9999;
+	PA_TOAST_TEXT.style.zIndex = 10000;
+
+	PA_TOAST_TEXT.style.opacity = 1.0;
+	PA_TOAST_BG.style.opacity = 1.0;
+}
+
+function pa_hide_toast() {
+	PA_TOAST_TEXT.style.opacity = 0.0;
+	PA_TOAST_BG.style.opacity = 0.0;
+	setTimeout(function() {
+		PA_TOAST_TEXT.style.zIndex = -1;
+		PA_TOAST_BG.style.zIndex = -1;
+	}, 1000);
+}
+
+/*
+change the colors of toast
+*/
+function pa_toast_set_style(bg, fg) {
+	PA_TOAST_TEXT.style.backgroundColor = bg;
+	PA_TOAST_TEXT.style.color = fg;
+
+	PA_TOAST_BG.style.backgroundColor = bg;
+	PA_TOAST_BG.style.color = fg;
+}
+
+/*
+Show a toast
+*/
+function pa_toast(message, msecs, bg, fg) {
+	if (_('pa_toast') == null) {
+		pa_get_toast_element(message);
+		PA_TOAST_BG = _('pa_toast_bg');
+		PA_TOAST_TEXT = _('pa_toast');
+	}
+	pa_toast_set_style(bg, fg);
+	pa_show_toast();
+	setTimeout(function() {
+		pa_hide_toast();
+	}, msecs);
+}
