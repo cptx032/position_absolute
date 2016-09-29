c8293250445dd66ab0a8d93b043a891898269092
Willie Lawrence <cptx032@gmail.com>
2016-09-29 13:03:36 -0300

    jquery animation

--- pa_left_menu.js
+++ pa_left_menu.js
@@ -4,7 +4,7 @@ Author: Willie Lawrence
 Email: cptx032 arroba gmail dot com
 */
 
-/// DEPENDS PA.JS
+/// DEPENDS PA.JS, JQUERY
 
 /*
 styles:
@@ -12,19 +12,22 @@ left: pa-lm
 hided: pa-lm-hide
 shown: pa-lm-show
 */
-
+var PA_LM_TIME = 600; // milisseconds
 function pa_lm_show(elem) {
-    elem.pa_dict.left = 0;
-    pa_add_class(elem, 'pa-lm-show');
-    pa_remove_class(elem, 'pa-lm-hide');
+    $(elem).animate({left:0}, PA_LM_TIME, function () {
+        pa_add_class(elem, 'pa-lm-show');
+        pa_remove_class(elem, 'pa-lm-hide');
+        elem.pa_dict.left = 0;
+        pa_update_elem(elem);
+    });
 
-    pa_update_elem(elem);
 }
 
 function pa_lm_hide(elem) {
-    elem.pa_dict.left = -1;
-    pa_add_class(elem, 'pa-lm-hide');
-    pa_remove_class(elem, 'pa-lm-show');
-
-    pa_update_elem(elem);
+    $(elem).animate({left:-window.innerWidth}, PA_LM_TIME, function () {
+        elem.pa_dict.left = -1;
+        pa_add_class(elem, 'pa-lm-hide');
+        pa_remove_class(elem, 'pa-lm-show');
+        pa_update_elem(elem);
+    });
 }
